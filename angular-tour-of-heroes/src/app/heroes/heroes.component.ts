6810181ff3d161b12c655345b8ff638500b0fa80
MariahAdams <mariah.adams23@yahoo.com>
2018-10-09 16:52:10 -0700

    wiring up heroes component view

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
+
+  hero = 'Windstorm';
 
   ngOnInit() {
+
   }
 
 }
